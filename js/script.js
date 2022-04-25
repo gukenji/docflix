c13b116e3c0dc011a8431f085e2ce12ed96aac09
Gustavo Takahashi <gustavokenjitakahashi@gmail.com>
2022-04-25 18:57:55 -0300

    incluido dropdown no link 'navegar' e na imagem de perfil e exclusao dos arquivos bootstrap (nao utilizado, feito via css puro).

--- js/script.js
+++ js/script.js
@@ -128,3 +128,27 @@ nextButton.addEventListener('mouseout', e => {
     nextButton.children[0].style.transform = "scale(1)";
 
 });
+
+
+
+
+
+
+
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches('[data-dropdown-button]');
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null ) return;
+   
+   let currentDropdown;
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.add('active');
+
+    }
+
+    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+        if (dropdown === currentDropdown) return;
+        dropdown.classList.remove('active');
+    })
+});
+
